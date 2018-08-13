c8080e22333a0d1e9cdf151d13878d88054d9b53
alexsilva <geniofuturo@gmail.com>
2018-08-13 18:53:57 -0300

    Fix magic setup.

--- magic_env.py
+++ magic_env.py
@@ -1,10 +1,19 @@
 import os
+import re
 
 import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 python_dir = os.path.dirname(sys.executable)
 
 magic_dirpath = os.path.join(python_dir, 'magic')
+
+# is virtualenv and installed ?
+if not os.path.isdir(magic_dirpath) and bool(re.match(".*Scripts", python_dir, re.I)):
+    magic_dirpath = os.path.join(os.path.dirname(os.path.dirname(script_dir)), "magic")  # Lib\\site-packages
+    if not os.path.isdir(magic_dirpath):
+        magic_dirpath = os.path.join(script_dir, "magic")
 
 _setup_loaded = False
 
