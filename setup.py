9553b64d82cf5de2ca02ec69af3833702cd38ca2
alexsilva <geniofuturo@gmail.com>
2023-10-26 14:29:32 -0300

    v1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ magic_dir = os.path.join(base_dir, 'magic')
 
 setup(
     name='magic-msys-bin',
-    version='1.4',
+    version='1.5',
     py_modules=['magic_env'],
     url='https://github.com/alexsilva/magic-msys-bin',
     scripts=[],
